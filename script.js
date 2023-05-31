3a98f7e5ea1d24ad1d8f68e7f16a63bc6ce014ee
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-05-31 14:24:48 -0700

    Add basic style

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let firstCard = 10;
-let secondCard = 7;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
 
-if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂");
-} else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack! 🥳");
-} else {
-  console.log("You're out of the game! 😭");
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
